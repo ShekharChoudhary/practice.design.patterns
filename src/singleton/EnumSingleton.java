9a8dc0f314e21c315e5efe3655db6a3e5ac337e8
Choudhary Shekhar <shekhar.choudhary@tieto.com>
2018-08-20 15:24:48 +0530

    added singleton code

---
+++ src/singleton/EnumSingleton.java
@@ -0,0 +1,24 @@
+package singleton;
+
+/**
+ * Only one instance of enum is created. 
+ * @author choudshe
+ *
+ */
+public class EnumSingleton {
+
+	public static void main(String[] args) {
+		
+		Singleton.INSTANCE.testMethod();
+		
+	}
+}
+
+
+enum Singleton{
+	INSTANCE;
+	
+	public void testMethod() {
+		System.out.println("This is enum singleton");
+	}
+}
