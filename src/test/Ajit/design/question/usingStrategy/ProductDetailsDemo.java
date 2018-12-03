8727bb39e5d0d30aa851af0442ed774dea9b040b
Choudhary Shekhar <shekhar.choudhary@tieto.com>
2018-12-03 22:54:30 +0530

    adding ajit design questions

---
+++ src/test/Ajit/design/question/usingStrategy/ProductDetailsDemo.java
@@ -0,0 +1,11 @@
+package test.Ajit.design.question.usingStrategy;
+/**
+ * Implementing this using Strategy pattern.
+ * 
+ * @author choudshe
+ *
+ */
+public class ProductDetailsDemo {
+
+	
+}
---
+++ src/test/Ajit/design/question/usingVisitor/ProductDetailsDemo.java
@@ -0,0 +1,10 @@
+package test.Ajit.design.question.usingVisitor;
+/**
+ * Implementing this using Visitor pattern.
+ * 
+ * @author choudshe
+ *
+ */
+public class ProductDetailsDemo {
+
+}
